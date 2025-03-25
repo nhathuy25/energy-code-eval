97b424f8dfabfd1a14e69c6de897b3fe6b6d1113
Huu Nhat Huy Nguyen <huunhathuy.nguyen@orange.com>
2025-03-25 14:19:16 +0100

    Initial commit

---
+++ code_eval/generation.py
@@ -0,0 +1,112 @@
+import json
+from math import ceil
+
+from typing import List, Optional
+import os
+
+from torch.utils.data.dataloader import DataLoader
+from transformers import StoppingCriteria, StoppingCriteriaList
+
+from code_eval.utils import TokenizedDataset, complete_code
+
+class EndOfFunctionCriteria(StoppingCriteria):
+    """Custom `StoppingCriteria` which checks if all generated functions in the batch are completed."""
+    def __init__(self, start_length, eof_strings, tokenizer, check_fn=None):
+        self.start_length = start_length
+        self.eof_strings = eof_strings
+        self.tokenizer = tokenizer
+        if check_fn is None:
+            check_fn = lambda decoded_generation: any(
+                [stop_string in decoded_generation for stop_string in self.eof_strings]
+            )
+        self.check_fn = check_fn
+
+    def __call__(self, input_ids, scores, **kwargs):
+        """Returns true if all generated sequences contain any of the end-of-function strings."""
+        decoded_generations = self.tokenizer.batch_decode(input_ids[:, self.start_length :])
+        return all([self.check_fn(decoded_generation) for decoded_generation in decoded_generations])
+
+class TooLongFunctionCriteria(StoppingCriteria):
+    """Custom `StoppingCriteria` which checks if the generated function is too long by a certain multiplier based on input length."""
+
+    def __init__(self, input_length, multiplier):
+        self.input_length = input_length
+        self.multiplier = multiplier
+
+    def __call__(self, input_ids, scores, **kwargs):
+        """Returns true if generated sequence is too long."""
+        return input_ids.shape[1] > int(self.input_length * self.multiplier)
+        
+def parallel_generations(
+        task,
+        dataset,
+        model,
+        tokenizer,
+        n_tasks,
+        args,
+        curr_sample_idx: int = 0,
+        intermediate_generations: Optional[List[Optional[List[Optional[str]]]]] = None,
+        intermediate_save_generations_path: Optional[str] = None,
+):
+    if args.load_generations_path:
+        # load generated code
+        with open(args.load_generations_path) as fp:
+            generations = json.load(fp)
+            print(
+                f"generations loaded, {n_tasks} selected from {len(generations)} with {len(generations[0])} candidates"
+            )
+        return generations[:n_tasks]
+    # Setup generation settings
+    gen_kwargs = {
+        "temperature": args.temperature,
+        "top_p": args.top_p,
+        "top_k": args.top_k,
+        "max_tokens": args.max_tokens,
+    }
+
+    # Instruction tokens is a set of 3 tokens: begin_token, end_token, assistant_token
+    # TODO: define the use for instruction tokens
+    if args.instruction_tokens:
+        instruction_tokens = args.instruction_tokens.split(",")
+        if len(instruction_tokens) != 3:
+            raise ValueError(
+                "Instruction tokens should contain exactly 3 tokens separated by a comma. If a token is empty, represent it as ''"
+            )
+        for token in instruction_tokens:
+            if token.strip() != "":
+                task.stop_words.append(token)
+    else:
+        instruction_tokens = None
+
+    # Define dataset loader for each line of task
+    ds_tokenized = TokenizedDataset(
+        task,
+        dataset,
+        tokenizer,
+        num_devices=args.tensor_parallel_size, #os.environ('SLURM_GPUS_PER_NODE'), TODO: define later for detect GPUs on current node
+        max_length=args.max_tokens,
+        limit_start=args.limit_start + curr_sample_idx,
+        n_tasks=n_tasks,
+        prefix=args.prefix,
+        instruction_tokens=instruction_tokens
+        )
+    
+    # Load dataset using torch.DataLoader, batch_size here is diff. from args.batch_size
+    ds_loader = DataLoader(ds_tokenized, batch_size=1)
+
+    generations = complete_code(
+        task,
+        model,
+        tokenizer,
+        ds_loader,
+        n_tasks,
+        limit_start=args.limit_start + curr_sample_idx,
+        batch_size=args.batch_size,
+        prefix=args.prefix,
+        instruction_tokens=instruction_tokens,
+        postprocess=args.postprocess,
+        intermediate_generations=intermediate_generations,
+        intermediate_save_generations_path=intermediate_save_generations_path,
+        **gen_kwargs
+    )
+    return generations
---
+++ code_eval/utils.py
@@ -0,0 +1,224 @@
+import json
+import math
+import re
+import warnings
+from collections import defaultdict
+from typing import List, Optional
+
+import torch
+from torch.utils.data import IterableDataset
+from tqdm import tqdm
+
+from vllm import SamplingParams
+from vllm.outputs import RequestOutput
+
+INFILL_MODE = False
+INSTRUCTION_MODE = False
+
+
+class TokenizedDataset(IterableDataset):
+    """Tokenize and preprocess the dataset
+    Multiple copies of the same prompt are sent sequentially. See compute_code for more details.
+    The prompt can either be:
+    - one prompt: normal code completion
+    - two prompts: for infilling mode (prefix, suffix) or instructin-tuning mode (instruction, context)
+    """
+
+    def __init__(
+        self,
+        task,
+        dataset,
+        tokenizer,
+        num_devices,
+        max_length,
+        limit_start=0,
+        n_tasks=None,
+        n_copies=1,
+        prefix="",
+        instruction_tokens=None,
+    ):
+        self.task = task
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.num_devices = num_devices
+        self.max_length = max_length
+        self.limit_start = limit_start
+        self.n_tasks = n_tasks
+        self.n_copies = n_copies
+        self.prefix = prefix
+        self.instruction_tokens = instruction_tokens
+
+    def __iter__(self):
+        prompts = []
+        infill = []
+        instruction = []
+        for sample in range(self.limit_start, self.limit_start + self.n_tasks):
+            prompt_contents = self.task.get_prompt(self.dataset[sample])
+            if isinstance(prompt_contents, str):
+                # Normal code completion mode
+                infill.append(False)
+                instruction.append(False)
+                prompt = self.prefix + prompt_contents
+            elif isinstance(prompt_contents, dict):
+                pass
+                # TODO: for code infilling and instruction-tuning mode
+            else:
+                raise ValueError(f"Unsupported prompt format: {type(prompt_contents)}")
+            
+            # Create list of prompts from the dataset 
+            prompts.append(prompt)
+
+        global INFILL_MODE
+        global INSTRUCTION_MODE
+        INFILL_MODE = infill[0]
+        INSTRUCTION_MODE = instruction[0]
+        if INFILL_MODE:
+            return_token_type_ids = False
+        else:
+            return_token_type_ids = None  # default
+
+        outputs = self.tokenizer(
+            prompts,
+            padding=True,
+            truncation=True,
+            return_tensors="pt", # input_ids in tensor type might deprecated in vLLM
+            max_length=self.max_length,
+            return_token_type_ids=return_token_type_ids
+        )
+
+        for sample in range(self.n_tasks):
+            for _ in range(self.n_copies):
+                yield {
+                    "content": str(prompts[sample]),
+                    "ids": outputs.input_ids[sample],
+                    "task_id": int(sample),
+                    "input_len": sum(outputs.attention_mask[sample])
+                }
+               
+        def _make_instruction_prompt(self, instruction, context, prefix=""):
+            pass
+            # TODO: Define later for prompt instruction fine-tunning
+
+def complete_code(
+    task,
+    model,
+    tokenizer,
+    dataloader,
+    n_tasks,
+    limit_start=0,
+    batch_size=20,
+    prefix="", 
+    instruction_tokens=None,
+    postprocess=False, #Later when defined, set to True by default
+    intermediate_generations: Optional[List[Optional[List[Optional[str]]]]] = None,
+    intermediate_save_generations_path: Optional[str] = None,
+    **gen_kwargs
+):
+    """
+    Complete the code base on dataset Loader, supported continuous generation and post processing step.
+    Return 
+    """
+    code_gens: List[List[Optional[str]]] = [[] for _ in range(n_tasks)]
+    generations = [] if not intermediate_generations else intermediate_generations
+    gen_token_dict = defaultdict(list) # dict of list of generated tokens
+    for step, batch in tqdm(
+        enumerate(dataloader),
+        total=math.ceil(
+            #n_tasks * dataloader.dataset.n_copies / num_processes # Define later for parallel distribution
+            n_tasks * dataloader.dataset.n_copies
+        ),
+    ):
+        input_tensors = batch["ids"][:, : batch["input_len"]] if tokenizer.padding_side == "right" else batch["ids"]
+        # Define the generations here
+        try:
+            #inputs = input_tensors.cpu().detach().numpy().tolist()
+            inputs = batch["content"]
+            sampling_params=SamplingParams(**gen_kwargs)
+            generated_outputs = model.generate(
+                prompts=inputs,
+                sampling_params=sampling_params,
+                use_tqdm=False
+            )
+        except ValueError as e:
+            raise e
+        # -- end generation --
+        generated_tasks = batch["task_id"].repeat(batch_size)
+        
+        for sample, generated_outputs in zip(generated_tasks, generated_outputs):
+            gen_token_dict[sample].append(generated_outputs)
+
+        if intermediate_save_generations_path and (step + 1) % 50 == 0:
+            # If exist path to save intermediate generations, save the generation every 50 tasks.
+            code_gens = update_code_gens(
+                task,
+                tokenizer,
+                limit_start,
+                prefix,
+                instruction_tokens,
+                postprocess,
+                code_gens,
+                gen_token_dict,
+            )
+            with open(intermediate_save_generations_path, "w") as fp:
+                json.dump(generations + code_gens, fp)
+                print(
+                    f"intermediate generations were saved at {intermediate_save_generations_path}"
+                )
+            # reset gen_token_dict - prevent redundant decoding
+            gen_token_dict = defaultdict(list)
+
+        # Adding and organised new generated outputs to 2D list 'code_gens' 
+        code_gens = update_code_gens(
+            task,
+            tokenizer,
+            limit_start,
+            prefix,
+            instruction_tokens,
+            postprocess,
+            code_gens,
+            gen_token_dict
+        )
+
+        generations.extend(code_gens)
+    return generations
+
+# TODO : define post processing step
+def update_code_gens(
+    task,
+    tokenizer,
+    limit_start,
+    prefix,
+    instruction_tokens,
+    postprocess,
+    code_gens,
+    gen_token_dict
+):
+    """
+    Take the generated outputs stocked in temporary 'gen_token_dict' and reorganised it in to 2D list of 'code_gens'.
+    Supported continuous generation with intermediate_generations and postprocessing step.
+    """
+    # TODO: To complete if postprocess is need
+    for sample, generated_outputs in gen_token_dict.items():
+        for s in generated_outputs:
+            if INFILL_MODE or tokenizer.eos_token in task.stop_words:
+                pass
+            # If generated code isn't in the output format of vLLM, process it
+            if not isinstance(s, RequestOutput):
+                gen_code = tokenizer.decode(
+                    s, skip_special_tokens=True
+                )
+            if isinstance(s, RequestOutput):
+                gen_code = generated_outputs[0].outputs[0].text  
+            else:
+                raise ValueError(
+                    "The outputs must be in the form of token ids or RequestOutput of vLLM"
+                )        
+            if not INFILL_MODE:
+                # TODO 
+                pass
+            if postprocess:
+                # TODO: define post processing step
+                # limit_start will be applied here
+                pass
+            code_gens[sample].append(gen_code)
+    return code_gens
